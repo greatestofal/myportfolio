b27ffb4a419bbd1af0526626d9099470e457dbf9
Adibe Nnamdi <adibennamdisolomon@gmail.com>
2024-04-19 17:29:48 +0100

    More edits

---
+++ src/components/Projects.jsx
@@ -0,0 +1,199 @@
+import React from "react";
+import MyEats from "../assets/projects/myeats.png";
+import DW from "../assets/projects/DW.png";
+import Netflix from "../assets/projects/netflix.png";
+import Zeus from "../assets/projects/zeus.png";
+import Nike from "../assets/projects/nike.png";
+import Advice from "../assets/projects/advice.png";
+
+const Projects = () => {
+  return (
+    <div
+      name="projects"
+      className="bg-[#0a192f] w-full md:h-screen text-gray-300"
+    >
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-[#64ffda] text-gray-300">
+            Projects
+          </p>
+          <p className="py-6"> Check Out Some Of My Works.</p>
+        </div>
+
+        {/* Grid  Container */}
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/* Grid Item */}
+
+          {/* DW */}
+          <div
+            style={{ backgroundImage: `url(${DW})` }}
+            className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                DataWeb Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://datawebs.vercel.app/" target="_blank">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/greatestofal/dataweb"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* MyEats */}
+          <div
+            style={{ backgroundImage: `url(${MyEats})` }}
+            className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Food Delivery App
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://myeats.vercel.app/" target="_blank">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/greatestofal/myeats"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Netflix */}
+          <div
+            style={{ backgroundImage: `url(${Netflix})` }}
+            className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Netflix Clone App
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://mynetflixcloneapp.vercel.app/" target="_blank">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/greatestofal/NetflixAppClone"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Zeus */}
+          <div
+            style={{ backgroundImage: `url(${Zeus})` }}
+            className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Web Store App
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://zeusstore.vercel.app/" target="_blank">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/greatestofal/fakestoreapi"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Nike */}
+          <div
+            style={{ backgroundImage: `url(${Nike})` }}
+            className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Nike App
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://nikecloneapp.vercel.app/" target="_blank">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://nikecloneapp.vercel.app/" target="_blank">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Advice App */}
+          <div
+            style={{ backgroundImage: `url(${Advice})` }}
+            className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Advice Generator App
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://myadvicegenerator.vercel.app/" target="_blank">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/greatestofal/Advice-Generator"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Container ends here */}
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,16 +14,66 @@ const Skills = () => {
   return (
     <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
       {/* Container */}
-      <div>
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
-          <p> Experience</p>
-          <p> These are the technologies I have worked with</p>
+          <p className="text-4xl font-bold inline border-b-4 border-[#64ffda]">
+            Skills
+          </p>
+          <p className="py-6">
+            These are some of the skills I have, and technologies I have worked
+            with.
+          </p>
         </div>
+        {/* Icon container */}
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img src={HTML} alt="HTML icon" className="w-20 mx-auto" />
+            <p className="my-4"> HTML</p>
+          </div>
 
-        <div>
-          <div>
-            <img src={HTML} alt="HTML icon" />
-            <p>HTML</p>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img src={CSS} alt="CSS icon" className="w-20 mx-auto" />
+            <p className="my-4"> CSS</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img
+              src={JavaScript}
+              alt="JavaScript icon"
+              className="w-20 mx-auto"
+            />
+            <p className="my-4"> JavaScript</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img src={ReactImg} alt="React icon" className="w-20 mx-auto" />
+            <p className="my-4"> REACT</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img src={Node} alt="Node icon" className="w-20 mx-auto" />
+            <p className="my-4"> NODE.js</p>
+          </div>
+          {/* <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img src={Firebase} alt="Firebase icon" className="w-20 mx-auto" />
+            <p className="my-4"> FIREBASE</p>
+          </div> */}
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img src={Tailwind} alt="Taiwind icon" className="w-20 mx-auto" />
+            <p className="my-4"> TAILWIND</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img
+              src={Wordpress}
+              alt="Wordpress icon"
+              className="w-20 mx-auto"
+            />
+            <p className="my-4"> WORDPRESS</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img
+              src={TechnicalWriting}
+              alt="HTML icon"
+              className="w-20 mx-auto"
+            />
+            <p className="my-4"> Technical Writing</p>
           </div>
         </div>
       </div>
