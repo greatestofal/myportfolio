1f0f074f366d71573f9e4f2a6fb20e05164cc6ce
Adibe Nnamdi <adibennamdisolomon@gmail.com>
2024-04-20 21:52:10 +0100

    Final

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,9 +4,13 @@ const Contact = () => {
   return (
     <div
       name="contact"
-      className="bg-[#0a192f] w-full md:h-screen flex justify-center items-center p-4"
+      className="bg-[#0a192f] w-full h-screen flex justify-center items-center p-4"
     >
-      <form action="" className="flex flex-col max-w-[600px] w-full">
+      <form
+        action="https://getform.io/f/pbgxvjka"
+        method="POST"
+        className="flex flex-col max-w-[600px] w-full"
+      >
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-[#64ffda] text-gray-300">
             Contact
@@ -32,9 +36,10 @@ const Contact = () => {
           name="message"
           rows="10"
           placeholder="Message"
-        >
-          {" "}
-        </textarea>
+        ></textarea>
+        <button className="text-white border-2 hover:bg-[#64ffda] hover:border-[#64ffda] hover:text-black hover:rounded hover:font-bold py-3 px-4 my-4 mx-auto flex items-center">
+          Let's Collaborate
+        </button>
       </form>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Logo from "../assets/logo.png";
+import { Link } from "react-scroll";
 import {
   FaBars,
   FaTimes,
@@ -19,11 +20,31 @@ const Navbar = () => {
       </div>
       {/* Menu */}
       <ul className=" hidden md:flex">
-        <li> Home </li>
-        <li> About </li>
-        <li> Skills </li>
-        <li> Projects </li>
-        <li> Contact </li>
+        <li>
+          <Link to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link to="projects" smooth={true} duration={500}>
+            Projects
+          </Link>
+        </li>
+        <li>
+          <Link to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
       </ul>
       {/* Hamburger */}
       {/* The md:hidden class will make the hamburger menu appear on screens smaller than 768px */}
@@ -40,11 +61,36 @@ const Navbar = () => {
             : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
         }
       >
-        <li className="py-6 text-4xl"> Home </li>
-        <li className="py-6 text-4xl"> About </li>
-        <li className="py-6 text-4xl"> Skills </li>
-        <li className="py-6 text-4xl"> Projects </li>
-        <li className="py-6 text-4xl"> Contact </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link
+            onClick={handleClick}
+            to="projects"
+            smooth={true}
+            duration={500}
+          >
+            Projects
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
       </ul>
       {/* Social Icons */}
       <div className=" hidden lg:flex fixed flex-col top-[35%] left-0">
