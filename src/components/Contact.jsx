82170e3fdcb74a0dd3272ae155577f42aa60ee3a
Adibe Nnamdi <adibennamdisolomon@gmail.com>
2024-04-22 11:25:50 +0100

    Worked on the contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import BasicForm from "../data/BasicForm";
 
 const Contact = () => {
   return (
@@ -6,41 +7,7 @@ const Contact = () => {
       name="contact"
       className="bg-[#0a192f] w-full h-screen flex justify-center items-center p-4"
     >
-      <form
-        action="https://getform.io/f/pbgxvjka"
-        method="POST"
-        className="flex flex-col max-w-[600px] w-full"
-      >
-        <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-[#64ffda] text-gray-300">
-            Contact
-          </p>
-          <p className="py-4 text-gray-300">
-            Submit the form below to get in touch with me.
-          </p>
-        </div>
-        <input
-          className=" bg-[#ccd6f6] p-2"
-          type="text"
-          name="name"
-          placeholder="Name"
-        />
-        <input
-          type="email"
-          name="name"
-          placeholder="Email"
-          className="my-4 p-2 bg-[#ccd6f6]"
-        />
-        <textarea
-          className="bg-[#ccd6f6] p-2"
-          name="message"
-          rows="10"
-          placeholder="Message"
-        ></textarea>
-        <button className="text-white border-2 hover:bg-[#64ffda] hover:border-[#64ffda] hover:text-black hover:rounded hover:font-bold py-3 px-4 my-4 mx-auto flex items-center">
-          Let's Collaborate
-        </button>
-      </form>
+      <BasicForm />
     </div>
   );
 };
---
+++ src/data/BasicForm.js
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+
+export default function BasicForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const [error, setError] = useState("");
+
+  function onSubmit(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    fetch("https://formcarry.com/s/EasfmcOJCle", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email, message }),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        if (response.code === 200) {
+          alert(
+            "Thanks for getting in touch. I will reach out to you soon. Thank you!"
+          );
+        } else if (response.code === 422) {
+          // Field validation failed
+          setError(response.message);
+        } else {
+          // other error from formcarry
+          setError(response.message);
+        }
+      })
+      .catch((error) => {
+        // request related error.
+        setError(error.message ? error.message : error);
+      });
+  }
+
+  return (
+    <form
+      onSubmit={(e) => onSubmit(e)}
+      className="flex flex-col max-w-[600px] w-full"
+    >
+      <div className="pb-8">
+        <p className="text-4xl font-bold inline border-b-4 border-[#64ffda] text-gray-300">
+          Get in touch
+        </p>
+        <p className="py-4 text-gray-300">
+          Submit the form below to get in touch with me.
+        </p>
+      </div>
+
+      <div className="flex flex-1 justify-between items-center"></div>
+      <div className="formcarry-block">
+        <label htmlFor="name"></label>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          id="name"
+          placeholder="First and Last Name"
+          className=" bg-[#ccd6f6] p-2"
+        />
+      </div>
+
+      <div className="formcarry-block">
+        <label htmlFor="email"></label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          id="email"
+          placeholder="john@doe.com"
+          className="my-4 p-2 bg-[#ccd6f6]"
+        />
+      </div>
+
+      <div className="formcarry-block">
+        <label htmlFor="message"></label>
+        <textarea
+          value={message}
+          name="message"
+          rows="10"
+          cols={"50"}
+          onChange={(e) => setMessage(e.target.value)}
+          id="message"
+          placeholder="Enter your message..."
+          className="bg-[#ccd6f6] p-2"
+        ></textarea>
+      </div>
+
+      <div className="formcarry-block">
+        <button
+          type="submit"
+          className="text-white border-2 hover:bg-[#64ffda] hover:border-[#64ffda] hover:text-black hover:rounded hover:font-bold py-3 px-4 my-4 w-[300px] items-center"
+        >
+          Send
+        </button>
+      </div>
+    </form>
+  );
+}
